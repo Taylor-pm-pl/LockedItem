71d4d3da5f58dc3212dc6f3c83623bd6069e8e78
JeroGamingYT <70517151+JeroGamingYT@users.noreply.github.com>
2021-08-30 21:29:45 +0700

    Update LockedItem.php

--- src/ytbjero/LockedItem/LockedItem.php
+++ src/ytbjero/LockedItem/LockedItem.php
@@ -12,6 +12,9 @@ use pocketmine\Server;
 use pocketmine\item\Item;
 use pocketmine\block\Block;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDropItemEvent;
 
@@ -20,6 +23,7 @@ use pocketmine\nbt\tag\StringTag;
 class LockedItem extends PluginBase implements Listener
 {
 	private static LockedItem $instance;
+	public $prefix = "§d§l⊹⊱§eLock Item§d⊰⊹";
 	public const KEY_VALUE = "§l§d[LOCKED]";
 
     public function onLoad() : void 
@@ -36,6 +40,47 @@ class LockedItem extends PluginBase implements Listener
 	  public static function getInstance(): LockedItem 
 	  {
         return LockedItem::$instance;
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, String $label, Array $args) : bool 
+    {
+    	if($command->getName() == "setlock"){
+    		if(!$sender->hasPermission("lockeditem.setlock")){
+    			$sender->sendMessage($this->prefix. " You don't have permisson to use this command!");
+    			return false;
+    		}
+    		$item = $sender->getInventory()->getItemInHand();
+    		if($item->getId() == 0){
+    			$sender->sendMessage($this->prefix. " You need to hold the item in your hand to lock it!");
+    			return false;
+    		}
+    		$item = $sender->getInventory()->getItemInHand();
+    		$item->setNamedTagEntry(new StringTag("Status", self::KEY_VALUE));
+    		$status[] = "[LOCKED]";
+		    $item->setLore($status);
+		    $sender->getInventory()->setItemInHand($item);
+		    $sender->sendMessage($this->prefix. " The item in your hand is locked!");
+		    return false;
+    	}
+    	if($command->getName() == "unlock"){
+    		if(!$sender->hasPermission("lockeditem.unlock")){
+    			$sender->sendMessage($this->prefix. " You don't have permisson to use this command!");
+    			return false;
+    		}
+    		$item = $sender->getInventory()->getItemInHand();
+    		if($item->getId() == 0){
+    			$sender->sendMessage($this->prefix. " You need to hold the item in your hand to unlock it!");
+    			return false;
+    		}
+    		$item = $sender->getInventory()->getItemInHand();
+    			$item->setNamedTagEntry(new StringTag("Status", "§l§d[UNLOCKED]"));
+    		$status[] = "[UNLOCKED]";
+		    $item->setLore($status);
+		    $sender->getInventory()->setItemInHand($item);
+		    $sender->sendMessage($this->prefix. " The item in your hand is unlocked!");
+		    return false;
+    	}
+    	return true;
     }
 
 	public function ItemMove(PlayerDropItemEvent $event) 
