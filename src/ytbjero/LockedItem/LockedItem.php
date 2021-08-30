f5f36ee9626e62e34d3cce28a04315e84785c662
JeroGamingYT <70517151+JeroGamingYT@users.noreply.github.com>
2021-08-30 19:38:52 +0700

    Update LockedItem.php

--- src/ytbjero/LockedItem/LockedItem.php
+++ src/ytbjero/LockedItem/LockedItem.php
@@ -16,11 +16,12 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDropItemEvent;
 
 use pocketmine\utils\TextFormat as TF;
-
+use pocketmine\nbt\tag\StringTag;
 class LockedItem extends PluginBase implements Listener
 {
 	private static LockedItem $instance;
 	public $prefix = "§d§l⊹⊱§eLock Item§d⊰⊹";
+	public const KEY_VALUE = "§l§d[LOCKED]";
 
     public function onLoad() : void 
     {
@@ -33,20 +34,28 @@ class LockedItem extends PluginBase implements Listener
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
-	  public static function getInstance(): LockedItem {
+	  public static function getInstance(): LockedItem 
+	  {
         return LockedItem::$instance;
     }
 
 	public function ItemMove(PlayerDropItemEvent $event) 
 	{
+		$item = $event->getItem();
 		$lore = $event->getItem()->getLore();
-		 if(in_array("\n§l§d[LOCKED]", $lore) or in_array("§l§d[LOCKED]", $lore) or in_array("\n§o§l§d[LOCKED]", $lore) or $lore == "§l§d[LOCKED]"){
+		if($item->getNameTag()->hasTag("Status")){
+		 if($item->getNameTag()->getString("Status") == self::KEY_VALUE){
 		 	$event->setCancelled(true);
 		 }
+		}
 	}
 
-	public function setLock($item)
+	public function setLocked(Item $item) : Item
 	{
-		$item->setLore(array("§l§d[LOCKED]"));
+		$item->setNameTagEntry(new StringTag("Status", self::KEY_VALUE));
+		$status = $item->getLore();
+		$status[] = "LOCKED";
+		$item->setLore($status);
+		return $item;
 	}
 }
