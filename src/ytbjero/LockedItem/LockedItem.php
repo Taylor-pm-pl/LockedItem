90a22b7f1dd8c541aa0e6bab5e1bb207542b6509
Jero Gaming <mw.sevice@gmail.com>
2021-09-01 13:56:51 +0700

    Update

--- src/ytbjero/LockedItem/LockedItem.php
+++ src/ytbjero/LockedItem/LockedItem.php
@@ -4,146 +4,146 @@ declare (strict_types=1);
 
 namespace ytbjero\LockedItem;
 
-use pocketmine\plugin\PluginBase;
-
 use pocketmine\Player;
 use pocketmine\Server;
 
-use pocketmine\item\Item;
-use pocketmine\block\Block;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerInteractEvent;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\plugin\PluginBase;
 
-use pocketmine\utils\TextFormat as TF;
+use pocketmine\block\Block;
+use pocketmine\item\Item;
+
 use pocketmine\nbt\tag\StringTag;
-use ytbjero\LockedItem\libs\JackMD\UpdateNotifier\UpdateNotifier;
-use pocketmine\utils\Config;
-class LockedItem extends PluginBase implements Listener
-{
-	private static $instance;
-	const KEY_VALUE = "Locked";
 
-	/** @var Config */
-	public $history;
-	public function onLoad() : void 
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+use ytbjero\LockedItem\libs\JackMD\UpdateNotifier\UpdateNotifier;
+class LockedItem extends PluginBase implements Listener{
+    
+    private static $instance;
+    
+    const KEY_VALUE = "isLocked";
+
+    public function onLoad() : void 
     {
         UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
     }
 
-	public function onEnable() : void
-	{
-		self::$instance = $this;
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->history = new Config($this->getDataFolder()."history.yml", Config::YAML);
-		if($this->getConfig()->getNested("History.Delete-onEnable") == true){
-			 foreach ($this->history->getAll() as $history => $data) {
-                $this->history->remove($history);
+    public function onEnable() : void
+    {
+        self::$instance = $this;
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+   
+    public static function getInstance() : self
+    {
+        return self::$instance;
+    }
+
+    public function onDrop(PlayerDropItemEvent $event) : void
+    {
+        $item = $event->getItem();
+        if ($this->isLocked($item)) {
+                $event->setCancelled(true);
+        }
+    }
+
+    public function onTouch(PlayerInteractEvent $event) : void
+    {
+            $player = $event->getPlayer();
+            $item = $player->getInventory()->getItemInHand();
+            $block = $event->getBlock();
+            if($block->getId() === Block::ITEM_FRAME_BLOCK){
+                if ($this->getConfig()->get("no-touch") == true) {
+                    if ($this->isLocked($item)) {
+                        $event->setCancelled(true);
+                        }
+                    }
+                }
             }
-            $this->history->save();
-		}
-	}
 
-	public static function getInstance() : self
-	{
-		return self::$instance;
-	}
+    public function onCommand(CommandSender $sender, Command $command, String $label, Array $args) : bool
+    {
+        if($command->getName() == "setlock") {
+            if(!$sender instanceof Player) {
+                $sender->sendMessage(TF::RED . "This command only works in game!");
+                return false;
+            }
+            if(!$sender->hasPermission("lockeditem.setlock")){
+                $sender->sendMessage($this->getConfig()->get("perm-message"));
+                return false;
+            }
+            $item = $sender->getInventory()->getItemInHand();
+            if($item->getId() == 0){
+                $sender->sendMessage($this->getConfig()->get("item-hand-message"));
+                return false;
+            }
+            $item = $sender->getInventory()->getItemInHand();
+                        if(!$this->isLocked($item)) {
+                            $sender->getInventory()->setItemInHand($this->setLock($item));
+                            $sender->sendMessage($this->getConfig()->get("lock-message"));
+                        } else{
+                        $sender->sendMessage($this->getConfig()->get("no-lock-message"));
+                        }
+                    }
+        if($command->getName() == "unlock") {
+            if(!$sender instanceof Player) {
+                $sender->sendMessage(TF::RED . "This command only works in game!");
+                return false;
+            }
+            if(!$sender->hasPermission("lockeditem.unlock")){
+                $sender->sendMessage($this->getConfig()->get("perm-message"));
+                return false;
+            }
+            $item = $sender->getInventory()->getItemInHand();
+            if($item->getId() == 0){
+                $sender->sendMessage($this->getConfig()->get("item-hand-message"));
+                return false;
+            }
+            $item = $sender->getInventory()->getItemInHand();
+                        if($this->isLocked($item)) {
+                           $sender->getInventory()->setItemInHand($this->unLock($item));
+                            $sender->sendMessage($this->getConfig()->get("unlock-message"));
+                        }else{
+                       $sender->sendMessage($this->getConfig()->get("no-unlock-message"));
+                }
+            }
+                return true;
+        }
 
-	public function onCommand(CommandSender $sender, Command $command, String $label, Array $args) : bool
-	{
-		if ($command->getName() == "setlock") {
-			if (!$sender->hasPermission("lockeditem.setlock")) {
-				$sender->sendMessage(TF::RED. " You don't have permisson to use this command!");
-				return false;
-			}
-			$item = $sender->getInventory()->getItemInHand();
-			if ($item->getNamedTag()->hasTag("Status", StringTag::class)) {
-				$sender->sendMessage(TF::RED . "Your item is already locked!");
-				return false;
-			}
-			if ($item->getId() == 0) {
-				$sender->sendMessage(TF::RED . "You need to hold the item in your hand to lock it!");
-				return false;
-			}
-			$item = $sender->getInventory()->getItemInHand();
-			$username = $sender->getName();
-			$item_name = $item->getName();
-			$day = date('d/m - H:i:s',time());
-			$item->setNamedTagEntry(new StringTag("Status", self::KEY_VALUE));
-			$status = $item->getLore();
-		    $status = ["LOCKED"];
-		    $item->setLore($status);
-			$sender->getInventory()->setItemInHand($item);
-					$txt = "$username lock the item with name $item_name.";
-					$this->history->set($day, $txt);
-					$this->history->save(); 
-			$sender->sendMessage(TF::GREEN . "The item in your hand is locked!");
-			return true;
-		}
-		if ($command->getName() == "unlock") {
-			if (!$sender->hasPermission("lockeditem.unlock")) {
-				$sender->sendMessage(TF::RED . "You don't have permisson to use this command!");
-				return false;
-			}
-			$item = $sender->getInventory()->getItemInHand();
-			if (!$item->getNamedTag()->hasTag("Status", StringTag::class)) {
-				$sender->sendMessage(TF::RED . "Can not! Item has not been locked before.");
-				return false;
-			}
-			if ($item->getId() == 0) {
-				$sender->sendMessage(TF::RED . "You need to hold the item in your hand to unlock it!");
-				return false;
-			}
-			$item->getNamedTag()->removeTag("Status");
-			$status = $item->getLore();
-			$username = $sender->getName();
-			$item_name = $item->getName();
-			$day = date('d/m - H:i:s',time());
-			unset($status[array_search(["LOCKED"], $status)]);
-			$item->setLore($status);
-			$sender->getInventory()->setItemInHand($item);
-					$txt = "$username unlock the item with name $item_name.";
-					$this->history->set($day, $txt);
-					$this->history->save(); 
-			$sender->sendMessage(TF::GREEN . "The item in your hand is unlocked!");
-			return true;
-		}
-		return true;
-	}
+    
+    public function isLocked(Item $item): bool
+    {
+        return $item->getNamedTag()->hasTag("Status", StringTag::class);
+    }
 
-	public function ItemMove(PlayerDropItemEvent $event)
-	{
-		$item = $event->getItem();
-		$player = $event->getPlayer()->getName();
-		$item_name = $item->getName();
-		if ($item->getNamedTag()->hasTag("Status")) {
-			if ($item->getNamedTag()->getString("Status") == self::KEY_VALUE) {
-				$event->setCancelled(true);
-					$day = date('d/m - H:i:s',time());
-					$txt = "$player have item with name $item_name is locked.";
-					$this->history->set($day, $txt);
-					$this->history->save(); 
-				}
-		} 
-		if($this->getConfig()->getNested("History.SaveUnlockItem") == true){
-			if (!$item->getNamedTag()->hasTag("Status", StringTag::class)) {
-				$day = date('d/m - H:i:s',time());
-					$txt = "$player have item with name $item_name is unlocked.";
-					$this->history->set($day, $txt);
-					$this->history->save(); 
-			}
-		}
-	}
-
-	public function setLocked(Item $item) : Item
-	{
-		$item->setNamedTagEntry(new StringTag("Status", self::KEY_VALUE));
-		$status = $item->getLore();
-		$status[] = "LOCKED";
-		$item->setLore($status);
-		return $item;
-	}
+    public function setLock(Item $item) : Item
+    {
+        if(!$this->isLocked($item)) {
+           $item->setNamedTagEntry(new StringTag("Status", self::KEY_VALUE));
+           $status = $item->getLore();
+           $status[] = $this->getConfig()->get("item-lore");
+           $item->setLore($status);   
+        }   
+        return $item;
+    }
+    
+    public function unLock(Item $item): Item
+    {
+        if($this->isLocked($item)) {
+           $item->getNamedTag()->removeTag("Status");
+           $status = $item->getLore();
+         unset($status[array_search($this->getConfig()->get("item-lore"), $status)]);
+         $item->setLore($status);
+           $item->setLore($item->getLore());
+        }
+        return $item;
+    }
 }
